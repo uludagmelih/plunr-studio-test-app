8ea84b308c31982aede74f727343a1fb7ae73703
uludagmelih <140753717+uludagmelih@users.noreply.github.com>
2026-08-22 17:36:02 -0400

    Added Test Page

--- config/plunr-modules.php
+++ config/plunr-modules.php
@@ -15,16 +15,6 @@ return array (
     'section' => 'Project',
     'pages' => 
     array (
-      'warehouses' => 
-      array (
-        'label' => 'Warehouses',
-        'route' => 'inventory.warehouses.index',
-        'path' => '/inventory/warehouses',
-        'page' => 'Inventory/Warehouses/Index',
-        'icon' => 'FileText',
-        'order' => 0,
-        'quick_access' => false,
-      ),
       'products' => 
       array (
         'label' => 'Products',
@@ -34,6 +24,29 @@ return array (
         'icon' => 'FileText',
         'order' => 0,
         'quick_access' => true,
+        'hidden' => false,
+      ),
+      'warehouses' => 
+      array (
+        'label' => 'Warehouses',
+        'route' => 'inventory.warehouses.index',
+        'path' => '/inventory/warehouses',
+        'page' => 'Inventory/Warehouses/Index',
+        'icon' => 'FileText',
+        'order' => 1,
+        'quick_access' => false,
+        'hidden' => false,
+      ),
+      'test' => 
+      array (
+        'label' => 'New page',
+        'route' => 'app.page',
+        'path' => '/page',
+        'page' => 'Page',
+        'icon' => 'FolderTree',
+        'order' => 2,
+        'quick_access' => false,
+        'hidden' => false,
       ),
     ),
   ),
